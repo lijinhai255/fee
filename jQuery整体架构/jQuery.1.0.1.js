959d8f8feeddbd1307c4ce35dcdd3ffda60a65d1
ickt_18_李金海 <976041476@qq.com>
2019-04-19 17:12:56 +0800

    jQuery整体框架

--- jQuery整体架构/jQuery.1.0.1.js
+++ jQuery整体架构/jQuery.1.0.1.js
@@ -1,12 +1,14 @@
 (function(root){
     var jQuery = function(){
-        return new jQuery.prototype.init();
+        return new jQuery.prototype.init();//享有jQuery原型对象
     }
     jQuery.prototype ={
         init:function(){
 
-        }
+        },
+        css:function(){}
     }
+    //
     //共享原型对象
     //jQuery的原型对象的init方法上的原型 = jQuery的原型
     jQuery.prototype.init.prototype = jQuery.prototype;
